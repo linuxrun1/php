b5041f5290476da13d0f309035105a9274b84293
I'm Unix <kaslamkaleng024@gmail.com>
2021-09-26 13:38:10 +0700

    Create db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect($_SERVER['MYSQLHOST'],$_SERVER['MYSQLUSER'],$_SERVER['MYSQLPASSWORD'],$_SERVER['MYSQLDATABASE']) or die(mysqli_error());
+$conn = mysqli_connect($_SERVER['MYSQLHOST'].':'.$_SERVER['MYSQLPORT'],$_SERVER['MYSQLUSER'],$_SERVER['MYSQLPASSWORD'],$_SERVER['MYSQLDATABASE']) or die(mysqli_error());
 
 ?>
